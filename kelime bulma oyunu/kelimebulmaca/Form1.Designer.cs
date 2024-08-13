951d28ee1fa6889b0d600613e3ee50c9ca751643
bertanoruc1 <75758480+bertanoruc1@users.noreply.github.com>
2024-08-13 18:04:29 +0300

    kelime oyunu
    kelime bulma oyunu

---
+++ kelime bulma oyunu/kelimebulmaca/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace kelimebulmaca
+{
+    partial class frmbul
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbasla = new System.Windows.Forms.Button();
+            this.lstbxkelimeler = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnbasla
+            // 
+            this.btnbasla.Location = new System.Drawing.Point(28, 15);
+            this.btnbasla.Margin = new System.Windows.Forms.Padding(4);
+            this.btnbasla.Name = "btnbasla";
+            this.btnbasla.Size = new System.Drawing.Size(153, 28);
+            this.btnbasla.TabIndex = 0;
+            this.btnbasla.Text = "Oyuna Başla";
+            this.btnbasla.UseVisualStyleBackColor = true;
+            this.btnbasla.Click += new System.EventHandler(this.btnbasla_Click);
+            // 
+            // lstbxkelimeler
+            // 
+            this.lstbxkelimeler.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstbxkelimeler.FormattingEnabled = true;
+            this.lstbxkelimeler.ItemHeight = 16;
+            this.lstbxkelimeler.Location = new System.Drawing.Point(28, 94);
+            this.lstbxkelimeler.Margin = new System.Windows.Forms.Padding(4);
+            this.lstbxkelimeler.Name = "lstbxkelimeler";
+            this.lstbxkelimeler.Size = new System.Drawing.Size(165, 420);
+            this.lstbxkelimeler.TabIndex = 1;
+            this.lstbxkelimeler.SelectedIndexChanged += new System.EventHandler(this.lstbxkelimeler_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kelimeler";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(232, 94);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(485, 417);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 537);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmbul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 569);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstbxkelimeler);
+            this.Controls.Add(this.btnbasla);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "frmbul";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kelime Bulmaca";
+            this.Load += new System.EventHandler(this.frmbul_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbasla;
+        private System.Windows.Forms.ListBox lstbxkelimeler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ kelime bulma oyunu/kelimebulmaca/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace kelimebulmaca
+{
+    public partial class frmbul : Form
+    {
+        public frmbul()
+        {
+            InitializeComponent();
+        }
+        string dosyayol, satir, kelime = "", butonname;
+        List<string> lstkelimler = new List<string>();
+        List<int> lstindexno = new List<int>();
+        List<Control> butonlar = new List<Control>();
+        List<Control> pasifbutonlar = new List<Control>();
+        public List<int> sayiuret(int kelimezunluk)
+        {
+            List<int> rastgelesayilar = new List<int>();
+            Random rnd = new Random();
+            int sayi, sayac = 0;
+            do
+            {
+                sayi = rnd.Next(0, kelimezunluk);
+                if (rastgelesayilar.IndexOf(sayi) == -1)
+                {
+                    rastgelesayilar.Add(sayi);
+                    sayac++;
+                }
+            } while (sayac != kelimezunluk);
+
+            return rastgelesayilar;
+        }
+
+        private void frmbul_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = string.Empty;
+            groupBox1.Controls.Clear();
+        }
+
+        private void lstbxkelimeler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public List<string> kelimeyaz()
+        {
+            List<string> yedekkelimler = new List<string>();
+            dosyayol = Directory.GetCurrentDirectory() + "\\kelimeler.txt";
+            FileStream dosyaerisim = new FileStream(dosyayol, FileMode.Open, FileAccess.Read);
+            StreamReader okuyucu = new StreamReader(dosyaerisim);
+            satir = okuyucu.ReadLine();
+            do
+            {
+                yedekkelimler.Add(satir.ToUpper());
+                satir = okuyucu.ReadLine();
+                if (satir == null)
+                {
+                    break;
+                }
+
+            } while (satir != null);
+
+            foreach (string item in yedekkelimler)
+            {
+                lstbxkelimeler.Items.Add(item);
+            }
+            return yedekkelimler;
+        }
+
+        private void btnbasla_Click(object sender, EventArgs e)
+        {
+            lstbxkelimeler.Items.Clear();
+            groupBox1.Controls.Clear();
+            label2.Text = "";
+            int harfno = 0;
+            List<char> lstharfler = new List<char>();
+            List<int> lstharfindexno = new List<int>();
+            lstkelimler = kelimeyaz();
+
+
+            for (int i = 0; i < lstkelimler.Count; i++)
+            {
+                lstindexno.Clear();
+                lstindexno = sayiuret(lstkelimler[i].Length);
+
+                foreach (int item in lstindexno)
+                {
+                    lstharfler.Add(lstkelimler[i][item]);
+                    //listBox1.Items.Add(lstkelimler [i][ item].ToString ());
+                }
+            }
+            lstharfindexno = sayiuret(90);
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button btnharf = new Button();
+                    btnharf.Height = 25;
+                    btnharf.Width = 25;
+                    btnharf.Text = lstharfler[lstharfindexno[harfno]].ToString();
+                    harfno++;
+                    btnharf.BackColor = Color.White;
+                    btnharf.Location = new Point(10 + (j * 35), 10 + (i * 35));
+                    btnharf.Name = (10 + (j * 35)).ToString() + (10 + (i * 35)).ToString();
+                    btnharf.Click += new EventHandler(btnharf_click);
+                    groupBox1.Controls.Add(btnharf);
+                }
+            }
+
+        }
+
+        public void btnharf_click(object sender, System.EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Enabled = false;
+            kelime += btn.Text;
+            if (kelime.Length > 7)
+            {
+                kelime = "";
+                kelime += btn.Text;
+            }
+
+            label2.Text = kelime;
+            butonname = btn.Location.X.ToString() + btn.Location.Y.ToString();
+            if (butonname == btn.Name)
+            {
+                butonlar.Add(btn);
+                pasifbutonlar.Add(btn);
+            }
+
+            if (lstkelimler.IndexOf(kelime) > -1)
+            {
+                lstkelimler.Remove(kelime);
+                kelime = "";
+
+                lstbxkelimeler.Items.Clear();
+                for (int i = 0; i < butonlar.Count; i++)
+                {
+                    butonlar[i].Enabled = false;
+                    butonlar[i].BackColor = Color.Red;
+                }
+                butonlar.Clear();
+                pasifbutonlar.Clear();
+                foreach (string item in lstkelimler)
+                {
+                    lstbxkelimeler.Items.Add(item);
+                }
+            }
+            else if (kelime.Length > 6)
+            { for (int i = 0; i < pasifbutonlar.Count; i++)
+                {
+                    pasifbutonlar[i].Enabled = true;
+                }
+                pasifbutonlar.Clear();
+                butonlar.Clear();
+            }
+
+
+        }
+    }
+}
